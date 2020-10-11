349171350c8cb5a55c6f84eca36f684e8b4e324f
dakaleyt <32375512+dakaleyt@users.noreply.github.com>
2020-10-11 18:43:25 -0400

    Added in method to do embed messages easily

--- commands/ping.py
+++ commands/ping.py
@@ -1,4 +1,8 @@
+from commands import _embedMessage
+import discord
 
-async def ping(msg):
+async def ping(ctx):
     print("pong")
-    await msg.channel.send("pong!")
+    message = _embedMessage.create("Ping Reply", "Pong!", "blue")
+    #_embedMessage.addField(message, "Imposter", "Red Sus", False)
+    await ctx.channel.send(embed=message)
