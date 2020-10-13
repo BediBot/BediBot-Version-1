c9d19b36830e356c11514331baae849908705a63
Zayd Tahir <zaydtahir20@gmail.com>
2020-10-13 14:15:26 -0400

    Removing Username From Hash Functions

--- commands/hashing-functions.py
+++ commands/hashing-functions.py
@@ -1,23 +1,20 @@
 import hashlib
 
 
-def hash_email(username, email):
+def hash_email(email):
     """
-    :param username: Discord username as string
     :param email: Uwaterloo email as string
     :return: Hex-String
     """
 
     sha256 = hashlib.sha256()
 
-    sha256.update(bytes(username, 'utf-8'))
     sha256.update(bytes(email, 'utf-8'))
     return sha256.hexdigest()
 
 
-def check_hash(username, email, stored_hash):
+def check_hash(email, stored_hash):
     """
-    :param username: Discord username as string
     :param email: Uwaterloo email as string
     :param stored_hash: Hex-String created from username and email when verified
     :return: Boolean
@@ -25,7 +22,6 @@ def check_hash(username, email, stored_hash):
 
     sha256 = hashlib.sha256()
 
-    sha256.update(bytes(username, 'utf-8'))
     sha256.update(bytes(email, 'utf-8'))
 
     return sha256.hexdigest() == stored_hash
