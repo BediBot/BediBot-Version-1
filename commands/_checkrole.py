fe6cc07fa742e483bc8294c992188869e5489351
dakaleyt <32375512+dakaleyt@users.noreply.github.com>
2020-10-17 21:04:23 -0400

    Checkrole function

---
+++ commands/_checkrole.py
@@ -0,0 +1,8 @@
+import discord
+
+def checkIfAuthorHasRole(ctx, role:str):
+    role = discord.utils.get(ctx.guild.roles, name = "role")
+    if role in ctx.author.roles:
+        return True
+    else:
+        return False
