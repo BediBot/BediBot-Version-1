800fafb24dccaa8632d4fbb077a608ba43624ad2
sahil-kale <32375512+sahil-kale@users.noreply.github.com>
2020-10-17 23:02:47 -0400

    Update _checkrole.py (#36)
    * Update _checkrole.py
    
    * Update _checkrole.py

--- commands/_checkrole.py
+++ commands/_checkrole.py
@@ -1,7 +1,7 @@
 import discord
 
-def checkIfAuthorHasRole(ctx, role:str):
-    role = discord.utils.get(ctx.guild.roles, name = "role")
+def checkIfAuthorHasRole(ctx, roleString:str):
+    role = discord.utils.get(ctx.guild.roles, name = roleString)
     if role in ctx.author.roles:
         return True
     else:
