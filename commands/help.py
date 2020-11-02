88044ed50f8db8d08162f8ac616c42af427c9026
sahil-kale <32375512+sahil-kale@users.noreply.github.com>
2020-11-02 14:08:06 -0700

    Update help.py

--- commands/help.py
+++ commands/help.py
@@ -4,17 +4,17 @@ commandPrefix2 = "$"
 
 
 async def helpCommand(ctx, client):
-    helpMessage = _embedMessage.create("Help Command", "Commands that can be run with BediBot. Each word represents an arguement", "green")
+    helpMessage = _embedMessage.create("Help Command", "Commands that can be run with BediBot. Each word represents an argument", "green")
     _embedMessage.addField(helpMessage, commandPrefix2 + "addQuote \"quote with spaces\" Name",
-                           "Add's a quote from Professor Bedi\nEx: " + commandPrefix2 + "addQuote \"Life is Good\", Bedi", False)
+                           "Adds a quote from the individual of your choice\nEx: " + commandPrefix2 + "addQuote \"Life is Good\", Bedi", False)
     _embedMessage.addField(helpMessage, commandPrefix2 + "confirm code",
                            "Allows you to enter in your 2FA verification code after you run the verify command\nEx: " + commandPrefix2 + "confirm 123456789", False)
     _embedMessage.addField(helpMessage, commandPrefix2 + "getQuotes person pagenumber",
-                           "Gets a person's quotes with a page number, with each page in 5 days\nEx: " + commandPrefix2 + "getQuote Bedi 2", False)
+                           "Gets a persons quotes with a page number, with each page in 5 days\nEx: " + commandPrefix2 + "getQuote Bedi 2", False)
     _embedMessage.addField(helpMessage, commandPrefix2 + "help", "Allows you to view commands!", False)
     _embedMessage.addField(helpMessage, commandPrefix2 + "ping", "Returns Pong", False)
     _embedMessage.addField(helpMessage, commandPrefix2 + "setbirthday YYYY MM DD",
-                           "Allows you to set your birthday and let the server know when to embarass you :D\nEx: " + commandPrefix2 + "setbirthday 2001 01 01", False)
+                           "Allows you to set your birthday and let the server know when to embarrass you :D\nEx: " + commandPrefix2 + "setbirthday 2001 01 01", False)
     _embedMessage.addField(helpMessage, commandPrefix2 + "unverify",
                            "Unverifies you from the server. Note that this does NOT remove the associated email address from your discord user ID", False)
     _embedMessage.addField(helpMessage, commandPrefix2 + "verify userID@uwaterloo.ca",
