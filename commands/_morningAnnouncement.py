3f1756a9c3210accfe4ca8c79c9c053a1dc1d191
zaydtahir <zaydtahir20@gmail.com>
2020-12-07 00:01:41 -0500

    Commenting out 8:35 edge case cuz it dont work (#50)

--- commands/_morningAnnouncement.py
+++ commands/_morningAnnouncement.py
@@ -52,5 +52,5 @@ async def check_if_morning_announcement_occurred_today(client):
 
 async def schedule_announcement(client):
     schedule.every().day.at("08:30").do(asyncio.run_coroutine_threadsafe, send_morning_announcement(client), client.loop)
-    schedule.every().day.at("08:35").do(asyncio.run_coroutine_threadsafe, check_if_morning_announcement_occurred_today(client), client.loop)
+    #schedule.every().day.at("08:35").do(asyncio.run_coroutine_threadsafe, check_if_morning_announcement_occurred_today(client), client.loop)
     schedule.every().minute.do(asyncio.run_coroutine_threadsafe, _dueDateMessage.edit_due_date_message(client), client.loop)
