13282164999b4b46e3a3faa45f219fdef0a5c692
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2020-12-19 15:57:46 -0500

    Say Command (#98)
    * Update _morningAnnouncement.py
    
    * Say Command (#97)
    
    Co-authored-by: Zayd Tahir <zaydtahir20@gmail.com>

---
+++ commands/say.py
@@ -0,0 +1,38 @@
+from commands import _embedMessage, _checkrole, _util
+
+
+async def say(ctx, client):
+    if not (_checkrole.author_has_role(ctx, "admin") or _checkrole.author_has_role(ctx, "admins()")):
+        replyEmbed = _embedMessage.create("Say Reply", "Invalid Permissions", "red")
+        await ctx.channel.send(embed = replyEmbed)
+        return
+
+    global target_channel
+
+    args = _util.parse_message(ctx.content)
+
+    if len(args) != 4:
+        await ctx.channel.send(embed = _embedMessage.create("Say Reply", "Invalid Syntax! You need three arguments for this function!", "red"))
+        return
+
+    title = args[1]
+
+    content = args[2]
+
+    channel_mention = args[3]
+
+    channels = client.get_all_channels()
+
+    for channel in channels:
+        if channel.mention == channel_mention:
+            target_channel = channel
+            break
+
+    if target_channel is None:
+        await ctx.channel.send(embed = _embedMessage.create("Say Reply", "Channel not Found!", "red"))
+        return
+
+    await ctx.delete()
+    await target_channel.send(embed = _embedMessage.create(title, content, "green"))
+
+    return
