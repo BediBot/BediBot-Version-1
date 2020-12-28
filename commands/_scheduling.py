8e0866f63101276313898c210ded47d51578301d
Zayd Tahir <zaydtahir20@gmail.com>
2020-12-28 15:48:35 -0500

    Various Settings Added
    Disabling Morning and Birthday Announcements, Disabling Due Dates, Number of Reactions for Quote Approval

--- commands/_scheduling.py
+++ commands/_scheduling.py
@@ -11,34 +11,41 @@ async def schedule_jobs(client):
 
     for guild in guild_list:
         guild_id = guild_list[guild]['settings']['guild_id']
-        channel_id = guild_list[guild]['settings']['channel_id']
+
+        try:
+            channel_id = client.get_guild(guild_id).get_channel(int(_mongoFunctions.get_settings(guild_id)['channel_id'])).id
+        except:
+            print("Channel ID not set or outdated")
+            continue
 
         guild_timezone = guild_list[guild]['settings']['timezone']
 
         scheduler.add_job(_util.purge_messages_in_channel, 'cron', hour = 23, minute = 59, second = 0, timezone = guild_timezone, args = [client, guild_id, channel_id])
 
-        birthday_time = guild_list[guild]['settings']['birthday_time'].split(':')
+        if guild_list[guild]['settings']['birthday_announcements_enabled']:
+            birthday_time = guild_list[guild]['settings']['birthday_time'].split(':')
 
-        scheduler.add_job(_birthdayMessage.send_birthday_message, 'cron', hour = int(birthday_time[0]), minute = int(birthday_time[1]), second = 1, timezone = guild_timezone,
-                          args = [client, guild_id, channel_id])
+            scheduler.add_job(_birthdayMessage.send_birthday_message, 'cron', hour = int(birthday_time[0]), minute = int(birthday_time[1]), second = 1, timezone = guild_timezone,
+                              args = [client, guild_id, channel_id])
 
-        announcement_time = guild_list[guild]['settings']['announcement_time'].split(':')
+        if guild_list[guild]['settings']['morning_announcements_enabled']:
+            announcement_time = guild_list[guild]['settings']['announcement_time'].split(':')
 
-        announcement_time_object = datetime.today()
-        announcement_time_object = announcement_time_object.replace(hour = int(announcement_time[0]), minute = int(announcement_time[1]))
+            announcement_time_object = datetime.today()
+            announcement_time_object = announcement_time_object.replace(hour = int(announcement_time[0]), minute = int(announcement_time[1]))
 
-        scheduler.add_job(_morningAnnouncement.send_morning_announcement, 'cron', hour = announcement_time_object.hour, minute = announcement_time_object.minute, second = 1,
-                          timezone = guild_timezone, args = [client, guild_id, channel_id])
-        announcement_time_object += timedelta(minutes = 5)
-        scheduler.add_job(_morningAnnouncement.check_if_morning_announcement_occurred_today, 'cron', hour = announcement_time_object.hour, minute = announcement_time_object.minute,
-                          second = 1, timezone = guild_timezone, args = [client, guild_id, channel_id])
+            scheduler.add_job(_morningAnnouncement.send_morning_announcement, 'cron', hour = announcement_time_object.hour, minute = announcement_time_object.minute, second = 1,
+                              timezone = guild_timezone, args = [client, guild_id, channel_id])
+            announcement_time_object += timedelta(minutes = 5)
+            scheduler.add_job(_morningAnnouncement.check_if_morning_announcement_occurred_today, 'cron', hour = announcement_time_object.hour,
+                              minute = announcement_time_object.minute,
+                              second = 1, timezone = guild_timezone, args = [client, guild_id, channel_id])
 
     scheduler.add_job(_dueDateMessage.edit_due_date_message, 'interval', minutes = 10, args = [client])
 
 
 async def reschedule_jobs(client):
     for job in scheduler.get_jobs():
-        print(job)
         job.remove()
 
     await schedule_jobs(client)
--- commands/settings.py
+++ commands/settings.py
@@ -10,8 +10,6 @@ async def settings(ctx, client):
     message = _embedMessage.create("Settings Reply", "Here are the settings for **" + ctx.guild.name + "**", "blue")
 
     _embedMessage.add_field(message, "Timezone", _mongoFunctions.get_settings(ctx.guild.id)['timezone'], False)
-    _embedMessage.add_field(message, "Verification Enabled?", _mongoFunctions.get_settings(ctx.guild.id)['verification_enabled'], False)
-
     try:
         channel_name = ctx.guild.get_channel(int(_mongoFunctions.get_settings(ctx.guild.id)['channel_id'])).mention
     except:
@@ -19,15 +17,27 @@ async def settings(ctx, client):
 
     _embedMessage.add_field(message, "Bedi Bot Channel", channel_name, False)
     _embedMessage.add_field(message, "Admin Role", _mongoFunctions.get_settings(ctx.guild.id)['admin_role'], False)
-    _embedMessage.add_field(message, "Daily Announcement Time", _mongoFunctions.get_settings(ctx.guild.id)['announcement_time'], False)
-    _embedMessage.add_field(message, "Daily Announcement Quote Author", _mongoFunctions.get_settings(ctx.guild.id)['announcement_quoted_person'], False)
-    _embedMessage.add_field(message, "Announcement Role", _mongoFunctions.get_settings(ctx.guild.id)['announcement_role'], False)
-    _embedMessage.add_field(message, "Birthday Announcement Time", _mongoFunctions.get_settings(ctx.guild.id)['birthday_time'], False)
-    _embedMessage.add_field(message, "Birthday Role", _mongoFunctions.get_settings(ctx.guild.id)['birthday_role'], False)
+    _embedMessage.add_field(message, "Verification Enabled?", _mongoFunctions.get_settings(ctx.guild.id)['verification_enabled'], False)
+    
+    _embedMessage.add_field(message, "Morning Announcements Enabled?", _mongoFunctions.get_settings(ctx.guild.id)['morning_announcements_enabled'], False)
+    if _mongoFunctions.get_settings(ctx.guild.id)['morning_announcements_enabled']:
+        _embedMessage.add_field(message, "Daily Announcement Time", _mongoFunctions.get_settings(ctx.guild.id)['announcement_time'], False)
+        _embedMessage.add_field(message, "Daily Announcement Quote Author", _mongoFunctions.get_settings(ctx.guild.id)['announcement_quoted_person'], False)
+        _embedMessage.add_field(message, "Announcement Role", _mongoFunctions.get_settings(ctx.guild.id)['announcement_role'], False)
+
+    _embedMessage.add_field(message, "Birthday Announcements Enabled?", _mongoFunctions.get_settings(ctx.guild.id)['birthday_announcements_enabled'], False)
+    if _mongoFunctions.get_settings(ctx.guild.id)['birthday_announcements_enabled']:
+        _embedMessage.add_field(message, "Birthday Announcement Time", _mongoFunctions.get_settings(ctx.guild.id)['birthday_time'], False)
+        _embedMessage.add_field(message, "Birthday Role", _mongoFunctions.get_settings(ctx.guild.id)['birthday_role'], False)
+
+    _embedMessage.add_field(message, "Due Dates Enabled?", _mongoFunctions.get_settings(ctx.guild.id)['due_dates_enabled'], False)
+    if _mongoFunctions.get_settings(ctx.guild.id)['due_dates_enabled']:
+        _embedMessage.add_field(message, "Streams", ', '.join(_mongoFunctions.get_settings(ctx.guild.id)['streams']), False)
+        _embedMessage.add_field(message, "Courses", ', '.join(_mongoFunctions.get_settings(ctx.guild.id)['courses']), False)
+        _embedMessage.add_field(message, "Due Date Types", ', '.join(_mongoFunctions.get_settings(ctx.guild.id)['due_date_types']), False)
+
     _embedMessage.add_field(message, "Quote Reaction Emoji Name", _mongoFunctions.get_settings(ctx.guild.id)['reaction_emoji'], False)
-    _embedMessage.add_field(message, "Streams", ', '.join(_mongoFunctions.get_settings(ctx.guild.id)['streams']), False)
-    _embedMessage.add_field(message, "Courses", ', '.join(_mongoFunctions.get_settings(ctx.guild.id)['courses']), False)
-    _embedMessage.add_field(message, "Due Date Types", ', '.join(_mongoFunctions.get_settings(ctx.guild.id)['due_date_types']), False)
+    _embedMessage.add_field(message, "Required Quote Reactions for Approval", _mongoFunctions.get_settings(ctx.guild.id)['required_quote_reactions'], False)
 
     await ctx.channel.send(embed = message)
 
