1e623ad24cd82716e6b72c337a46d000254c24e8
Zayd Tahir <zaydtahir20@gmail.com>
2021-01-05 21:50:27 -0500

    Count all members in Ping command

--- commands/ping.py
+++ commands/ping.py
@@ -4,5 +4,9 @@ from commands import _embedMessage
 
 
 async def ping(ctx: discord.Message, client: discord.Client):
-    message = _embedMessage.create("Ping Reply", "Pong! **{0}ms**".format(int(client.latency * 1000)), "blue")
+    member_count = 0
+    for member in client.get_all_members():
+        member_count += 1
+
+    message = _embedMessage.create("Ping Reply", "Pong! **{0}ms**\n Users: **{1}**".format(int(client.latency * 1000), member_count), "blue")
     await ctx.channel.send(embed = message)
