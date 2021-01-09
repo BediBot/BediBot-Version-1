86557508ffe9f30fa78320265296807a0c629eaf
Zayd Tahir <zaydtahir20@gmail.com>
2021-01-09 17:04:10 -0500

    Stats status (#176)
    * Organizing Help Command
    
    * Reorganizing Help
    
    * AddDueDate no longer requires admin role by default
    
    This will be made a setting later on, but for now, any
    
    * Fixing Typo
    
    * Adding setup to help
    
    * Add guild and user stats to possible bot statuses

--- commands/_util.py
+++ commands/_util.py
@@ -23,3 +23,17 @@ def author_is_bot_owner(ctx: discord.Message) -> bool:
 
 async def purge_messages_in_channel(client: discord.Client, guild_id: int, channel_id: int):
     await client.get_guild(guild_id).get_channel(channel_id).purge(limit = None, check = lambda msg: not msg.pinned)
+
+
+def get_guild_count(client: discord.Client):
+    return len(client.guilds)
+
+
+def get_member_count(client: discord.Client):
+    member_count = 0
+
+    for user in client.get_all_members():
+        if not user.bot:
+            member_count += 1
+
+    return member_count
