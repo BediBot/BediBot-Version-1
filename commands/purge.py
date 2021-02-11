b5b2d995c74765d503dd728e2a6fcd485e5d2ca6
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2021-02-11 14:34:13 -0500

    Add in purge limit

--- commands/purge.py
+++ commands/purge.py
@@ -2,6 +2,8 @@ import discord
 
 from commands import _util, _checkrole, _mongoFunctions, _embedMessage
 
+MAX_PURGE_LIMIT = 50
+
 async def purge(ctx: discord.Message, client: discord.Client):
     #Check if user has admin permissions
     if not (_checkrole.author_has_role(ctx, _mongoFunctions.get_settings(ctx.guild.id)['admin_role']) or _util.author_is_bot_owner(ctx)):
@@ -12,8 +14,8 @@ async def purge(ctx: discord.Message, client: discord.Client):
     args = _util.parse_message(ctx.content)
     numberOfMessages = int(args[1])
 
-    if(len(args) != 2 or numberOfMessages < 1):
-        await ctx.channel.send(embed = _embedMessage.create("Purge Reply", "Invalid Syntax! You need 1 argument for this function that is a positive integer!\nSyntax: purge numberOfMessages", "red"))
+    if(len(args) != 2 or numberOfMessages < 1 or numberOfMessages > MAX_PURGE_LIMIT):
+        await ctx.channel.send(embed = _embedMessage.create("Purge Reply", "Invalid Syntax! You need 1 argument for this function that is a positive integer less than " + str(MAX_PURGE_LIMIT) + "!\nSyntax: purge numberOfMessages", "red"))
         return
 
     
