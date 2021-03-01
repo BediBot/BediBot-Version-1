d7695e07bc512997636b41fd183b935f3590a86e
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2021-03-01 02:54:45 -0500

    Update say.py

--- commands/say.py
+++ commands/say.py
@@ -14,31 +14,34 @@ async def say(ctx: discord.Message, client: discord.Client):
 
     args = _util.parse_message(ctx.content)
 
-    if len(args) != 4:
-        await ctx.channel.send(embed = _embedMessage.create("Say Reply", "Invalid Syntax! You need three arguments for this function!\nSyntax: say title content channel", "red"))
+    if len(args) != 4 and len(args) != 3:
+        await ctx.channel.send(embed = _embedMessage.create("Say Reply", "Invalid Syntax! You need three arguments for this function!\nSyntax: say title content OPTIONAL:channel", "red"))
         return
 
     title = args[1]
 
     content = args[2]
 
-    channel_mention = args[3]
+    target_channel = ctx.channel
+    if len(args) == 4:
+        channel_mention = args[3]
+        channels = client.get_all_channels()
 
-    channels = client.get_all_channels()
+        for channel in channels:
+            if channel.mention == channel_mention:
+                target_channel = channel
+                break
 
-    for channel in channels:
-        if channel.mention == channel_mention:
-            target_channel = channel
-            break
+        if target_channel is None:
+            await ctx.channel.send(embed = _embedMessage.create("Say Reply", "Channel not Found!", "red"))
+            return
 
-    if target_channel is None:
-        await ctx.channel.send(embed = _embedMessage.create("Say Reply", "Channel not Found!", "red"))
-        return
+    
 
     try:
         await ctx.delete()
     except:
-        print("Missing Manage Messages permission in {0} on server ID: {1}".format(channel.mention, str(ctx.guild.id)))
+        print("Missing Manage Messages permission in {0} on server ID: {1}".format(channel.mention, client.get_guild(ctx.guild.id)))
 
     if not _util.author_is_bot_owner(ctx):
         embed = _embedMessage.create(title, content + "\n\n This message was sent by {}".format(ctx.author.mention), "green")
